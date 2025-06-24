8199b27913be0d419886c94709ba89c45cb8d54e
vishnupriya760 <vishnupriyaot7@gmail.com>
2025-06-24 15:53:45 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './component/Home';
+
+function App() {
+  return (
+    <div>
+      <Home />
+      </div>
+  );
+}
+
+export default App;
---
+++ Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <div className='ko'>
+            <p className='g'>Home</p>
+            <p>About</p>
+            <p>Contact</p>
+            <button className='ki'>Submit</button>
+        </div>
+    </div>
+  )
+}
+
+export default Home
